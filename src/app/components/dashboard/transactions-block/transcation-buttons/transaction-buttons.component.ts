bb7bdfd81bf538b91f2c780e3cab154f6be866fd
Lishchuk Bohdan <121406264+Jwoliv@users.noreply.github.com>
2024-07-18 19:21:09 +0300

    add transaction page

--- src/app/components/dashboard/transactions-block/transcation-buttons/transaction-buttons.component.ts
+++ src/app/components/dashboard/transactions-block/transcation-buttons/transaction-buttons.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-transcation-buttons',
+    selector: 'app-transaction-buttons',
   standalone: true,
   imports: [],
   templateUrl: './transaction-buttons.component.html',
--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {Transaction} from "../../model/transaction.model";
+import {TransactionItemComponent} from "../../shared/components/transaction-item/transaction-item.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [],
+  imports: [
+    TransactionItemComponent,
+    NgForOf
+  ],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.scss'
 })
 export class TransactionsComponent {
-
+  public transactions: Transaction[] = [
+    {id: 1, category: 'Salary', type: 'EARNING', amount: 300, date: new Date(), account: {id: 1, name: 'cash'}},
+    {id: 1, category: 'Salary', type: 'SPENDING', amount: 300, date: new Date(), account: {id: 1, name: 'cash'}},
+    {id: 1, category: 'Salary', type: 'SPENDING', amount: 300, date: new Date(), account: {id: 1, name: 'cash'}},
+    {id: 1, category: 'Salary', type: 'EARNING', amount: 300, date: new Date(), account: {id: 1, name: 'cash'}},
+    {id: 1, category: 'Salary', type: 'SPENDING', amount: 300, date: new Date(), account: {id: 1, name: 'cash'}},
+    {id: 1, category: 'Salary', type: 'SPENDING', amount: 300, date: new Date(), account: {id: 1, name: 'cash'}},
+    {id: 1, category: 'Salary', type: 'EARNING', amount: 300, date: new Date(), account: {id: 1, name: 'cash'}},
+    {id: 1, category: 'Salary', type: 'SPENDING', amount: 300, date: new Date(), account: {id: 1, name: 'cash'}},
+  ]
 }
