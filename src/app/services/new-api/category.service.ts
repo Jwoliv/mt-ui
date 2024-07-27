1636d386f5eac97142382e756cae9d5287917469
Jwoliv <bohdanlishchuk0@gmail.com>
2024-07-27 16:42:57 +0300

    remove unused import

--- src/app/services/new-api/category.service.ts
+++ src/app/services/new-api/category.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {getBasePathUrl, getBasePathUrl2} from "../config/properties.config";
+import {getBasePathUrl2} from "../config/properties.config";
 import {HttpClient} from "@angular/common/http";
 import {CategoryFormDto} from "../../model/category.model";
 import {AuthService} from "../auth/auth.service";
